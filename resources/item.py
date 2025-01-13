58b85d36bc5fa86c2b3c5512fc4c85a5a0dec22d
Mohan Nepali <nepalimohan@outlook.com>
2025-01-13 12:26:59 +0545

    feat: items.py

--- resources/item.py
+++ resources/item.py
@@ -1,11 +1,24 @@
-from flask_restful import Resource
+from flask_restful import Resource, reqparse
+
+items = []
 
 class Item(Resource):
+    parser = reqparse.RequestParser()
+    parser.add_argument('price', type=float, require=True, help="This field cannot be left blank!")
+    
     def get(self, name):
-        return {'item': name}, 200
+        for item in items:
+            if item['name'] == name:
+                return item, 200
+        return {'message': 'Item not found'}, 200
     
     def post(self, name):
-        return {'message': f'Item {name} created'}, 201
+        data = Item.parser.parse_args()
+        item = {'name': name, 'price': data['price']}
+        items.append(item) 
+        return item, 201
     
     def delete(self, name):
+        global items
+        items = [item for item in items if item['name'] != name]
         return {'message': f'Item {name} deleted'}, 200
